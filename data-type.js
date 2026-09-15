eb5ae4995bf6e37a8900246b737e7fc9157a306c
Siddikaa <rojisid786@gmail.com>
2026-09-15 22:12:23 +0530

    PW-First-commit

---
+++ data-type.js
@@ -0,0 +1,29 @@
+// Primitive data type//
+
+//Number
+
+let n1 = 10;
+let n2 = 20;
+
+// String
+
+let name = "Aaysha Siddika";
+
+// boolean
+let isLoggedIn = true;
+
+// undefine
+
+let result;
+console.log(result);
+
+// null
+let data = null;
+
+//bigInt
+
+let bigInt = 23442355657n;
+
+// Symbol
+
+let id=Symbol("id");
---
+++ hello-world.js
@@ -0,0 +1,8 @@
+console.log("Hello")
+    let name= "Aaysha"
+    var n =10
+    function add(n1, n2){
+        var result= n1+n2
+        return result;
+    }
+    var value= add(10, 20)
