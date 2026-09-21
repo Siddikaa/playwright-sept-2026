39a80885a077d8caf3eab44e935cfa1f1cba8012
Siddikaa <rojisid786@gmail.com>
2026-09-21 13:00:29 +0530

    update multi

--- hello-world.js
+++ hello-world.js
@@ -1,9 +1,9 @@
 console.log("Hello")
     let name= "Aaysha"
     var n =10
-    function div(n1, n2){
-        var result= n1/n2
+    function multi(n1, n2){
+        var result= n1*n2
         return result;
     }
-    var value= div(10, 20)
-     var value1= div(10, 20)
+    var value= multi(10, 20)
+     var value1= multi(10, 20)
