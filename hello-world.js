9d4fbca6d464acad298bf8ae9a3955024ed0ea51
Siddikaa <rojisid786@gmail.com>
2026-09-24 20:51:11 +0530

    Merge conflict

--- hello-world.js
+++ hello-world.js
@@ -1,10 +1,10 @@
 console.log("Hello")
     let name= "Aaysha"
     var n =10
-    function div(n1, n2){
-        var result= n1/n2
+    function Sub(n1, n2){
+        var result= n1-n2
         return result;
     }
     console.log(name)
-    var value= div(10, 20)
-     var value1= div(10, 20)
+    var value= Sub(10, 20)
+     var value1= Sub(10, 20)
