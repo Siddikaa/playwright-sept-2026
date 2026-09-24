0ebda0a6bcc26b0b2a47095fdf53fc81ebecdf31
Siddikaa <rojisid786@gmail.com>
2026-09-24 21:03:33 +0530

    file updated

--- hello-world.js
+++ hello-world.js
@@ -1,10 +1,10 @@
 console.log("Hello")
     let name= "Aaysha"
-    var n =10
+    var n =1034554
     function Sub(n1, n2){
         var result= n1-n2
         return result;
     }
-    console.log("Name:", name)
-    var value= Sub(10, 20)
+    console.log("Value is = :", name)
+    var value= Sub(100, 220)
      var value1= Sub(10, 20)
