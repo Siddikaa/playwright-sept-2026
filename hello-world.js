8b6437ad73fe1e5f5ebf1f537271f6fb1b64ea5d
Siddikaa <rojisid786@gmail.com>
2026-09-24 20:53:42 +0530

    update file

--- hello-world.js
+++ hello-world.js
@@ -5,6 +5,6 @@ console.log("Hello")
         var result= n1-n2
         return result;
     }
-    console.log(name)
+    console.log("Name:", name)
     var value= Sub(10, 20)
      var value1= Sub(10, 20)
